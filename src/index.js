d46125bcf369ead9f59727ff09b2188310c74cbd
Андрій Симонов <Symonov.A@simpatik.kyiv>
2020-10-06 21:00:00 +0300

    framework's functional is ended

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import { Excel } from './components/excel/Excel'
+import { Table } from './components/table/Table'
+import { Formula } from './components/formula/Formula'
+import { Header } from './components/header/Header'
+import { Toolbar } from './components/toolbar/Toolbar'
 import './scss/index.scss'
+
+const excel = new Excel('#app', {
+	components: [Header, Toolbar, Formula, Table],
+})
+excel.render()
